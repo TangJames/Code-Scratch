795a00f6c6f913cf90914c1163a81de074d35b88
Devin Ryan <devin.ryan7@gmail.com>
2017-11-06 14:42:08 -0600

    adding functionality to code snippet component

--- src/components/CodeSnippet.js
+++ src/components/CodeSnippet.js
@@ -3,11 +3,38 @@ import React, { Component } from 'react';
 
 
 class CodeSnippet extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state={
+      value: ''
+    };
+
+    this.getInitialState = this.getInitialState.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  getInitialState() {
+    this.setState({
+    return {value: 'Enter your code scratch here'};
+    });
+  }
+
+  handleChange(event) {
+    event.preventDefault();
+    this.setState({value: event.target.value});
+  }
+
   render() {
     return (
-      <div className="CodeSnippet">
-        <h2> Hi2 </h2>
-      </div>
+        <div className="CodeSnippet">
+          <h2> Hi2 </h2>
+              <input
+                type="text"
+                value={this.state.value}
+                onChange={this.handleChange}
+              />
+        </div>
     );
   }
 }
