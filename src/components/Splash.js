402d61a81cb8399803a2c6cb2ce59a7f5c7e4711
James Tang <ulrichyu@outie.net>
2017-11-06 16:25:46 -0600

    github login

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,36 +1,67 @@
 import React, { Component } from 'react';
 import logo_black from '../logo_black.png';
-import LoginButton from './LoginButton.js';
 
 
 
+const Welcome = ({user, onSignOut})=> {
 
+  return (
+    <div>
+      Welcome <strong>{user.username}</strong>!
+      <a href="javascript:;" onClick={onSignOut}>Sign out</a>
+    </div>
+  )
+}
+
+class LoginForm extends Component {
+
+  handleSignIn(e) {
+    e.preventDefault()
+    let username = this.refs.username.value
+    let password = this.refs.password.value
+    this.props.onSignIn(username, password)
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSignIn.bind(this)}>
+        <h3>Sign in</h3>
+        <input type="text" ref="username" placeholder="enter you username" />
+        <input type="password" ref="password" placeholder="enter password" />
+        <input type="submit" value="Login" />
+      </form>
+    )
+  }
+
+}
 
 
 class Splash extends Component {
 
-  getProfileInfo() {
-    if (this.props.currentUser) {
-      // user is logged in
-      let displayName;
-      if (this.props.currentUser.displayName && this.props.currentUser.displayName.length > 0) {
-        displayName = this.props.currentUser.displayName;
-      } else {
-        displayName = this.props.currentUser.email;
-      }
-      return (
-        <div>
-          <h1>{displayName}</h1>
-          <img className="navbar-profile-pic" src={this.props.currentUser.photoURL} alt="" />
-        </div>
-      );
-    } else {
-      // user is not logged in
-      return <LoginButton>Log in with GitHub</LoginButton>;
+  constructor(props) {
+    super(props)
+    // the initial application state
+    this.state = {
+      user: null
     }
   }
 
+  // App "actions" (functions that modify state)
+  signIn(username, password) {
+    // This is where you would call Firebase, an API etc...
+    // calling setState will re-render the entire app (efficiently!)
+    this.setState({
+      user: {
+        username,
+        password,
+      }
+    })
+  }
 
+  signOut() {
+    // clear out user from state
+    this.setState({user: null})
+  }
 
   render() {
     // Here we pass relevant state to our child components
@@ -39,7 +70,17 @@ class Splash extends Component {
     return (
       <div>
         <img src={logo_black} className="App-logo" alt="logo" />
-        { this.getProfileInfo() }
+        {
+          (this.state.user) ?
+            <Welcome
+             user={this.state.user}
+             onSignOut={this.signOut.bind(this)}
+            />
+          :
+            <LoginForm
+             onSignIn={this.signIn.bind(this)}
+            />
+        }
       </div>
     )
 
