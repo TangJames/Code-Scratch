f13828d27db20109fd4dc9e4ecde55b9e116c76d
Jesse Lewis <jesseaustinlewis@gmail.com>
2017-11-07 11:44:22 -0600

    implementing a compononent redirect by logged in condition. moved logged in state from splash to HomePage and created a Results component

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,15 +1,48 @@
 import React, { Component } from 'react';
 import Search from './Search.js';
+import Results from './Results.js';
 import CodeSnippet from './CodeSnippet.js';
+import LoginButton from './LoginButton.js';
 
 
 //<Results currentUser={ this.state.currentUser } /> line: 12
 class HomePage extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      currentUser: this.props.currentUser,
+    }
+  }
+
+  getProfileInfo() {
+    if (this.props.currentUser) {
+
+     // user is logged in
+    let displayName;
+    if (this.props.currentUser.displayName && this.props.currentUser.displayName.length > 0) {
+      displayName = this.props.currentUser.displayName;
+    } else {
+      displayName = this.props.currentUser.email;
+    }
+    return (
+      <div>
+        <h1>{displayName}</h1>
+        <img className="navbar-profile-pic" src={this.props.currentUser.photoURL} alt="" />
+      </div>
+    );
+  } else {
+    return <LoginButton />;
+  }
+  }
+
   render() {
     return (
-      <div className="HomePage">
-        <Search currentUser={ this.state.currentUser }  />
 
+      <div className="HomePage">
+       { this.getProfileInfo() }
+        <Search currentUser={ this.state.currentUser }  />
+        <Results currentUser={ this.state.currentUser } />
       </div>
     );
   }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,23 @@
 // This file goes into homePage.js
 import React, { Component } from 'react';
 
-
 class Search extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      currentUser: this.props.currentUser,
+      searchKeyword: null,
+    }
+  }
+
   render() {
     return (
       <div className="Search">
-        <h1> Hi3 </h1>
+        <form name="searchBox">
+          <input id="searchKeys" placeholder="Search Your Code Snippets" />
+          <button type="submit">submit</button>
+        </form>
       </div>
     );
   }
